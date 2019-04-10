c862fdcfed2ade24e17f98e907a3ba4f37657848
Estudiante <Estudiante@BE401S103C09ING.unal.loc>
2019-04-10 08:58:14 -0500

    version 1

---
+++ src/Dog.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Dog {
+    public String nombre;
+    public int Peso;
+    public int Edad;
+    public String Color;
+    public void comer(){
+    }
+    public void correr(){
+    }
+    public void dormir(){
+    }
+    public void ladrar(){
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    public static void main(String[] args) {
+        Dog Miguel =new Dog();
+        Miguel.nombre="Miguel";
+      System.out.println(Miguel.nombre);
+      
+    }
+    
+         
+}
